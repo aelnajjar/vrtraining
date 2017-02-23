b737c0b0eca6d86701edea2e3134dd96d64e992e
aelnajjar <a.elnajjar@hotmail.com>
2017-02-23 18:59:35 +0400

    Update server.php

--- server.php
+++ server.php
@@ -46,10 +46,12 @@ $MySQLi_CON->close();
 		  
           </ul>
         </div><!--/.nav-collapse -->
+	      "\r\n"
       </div>
+	"\r\n"
     </nav>
 		
-	"\r \n \n \n \n"
+	"\r\n"
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
