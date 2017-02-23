662e468b4a9557082a882c9157b38bbfebccc2ed
aelnajjar <a.elnajjar@hotmail.com>
2017-02-23 19:11:30 +0400

    Update server.php

--- server.php
+++ server.php
@@ -49,9 +49,9 @@ $MySQLi_CON->close();
       </div>
     </nav>
 		
+	echo "\n";
+	echo "\n";
 	
-	
-	"\r\n"
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
