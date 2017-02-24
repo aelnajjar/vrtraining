b65b1c85e25fd806e1c3f89cf5ac727d9b8b1d0d
aelnajjar <a.elnajjar@hotmail.com>
2017-02-24 16:40:48 +0400

    Update gameinfo.php

--- gameinfo.php
+++ gameinfo.php
@@ -25,7 +25,7 @@ $username = $_GET["user"];
 $password = $_GET["password"];
 
 // create a prepared statement
-$stmt = mysqli_prepare($mysqli, "SELECT username, password, regkey, banned FROM users1 WHERE username='$username'");
+$stmt = mysqli_prepare($mysqli, "SELECT username, password, regkey, banned FROM users WHERE username='$username'");
 
 // bind parameters
 mysqli_stmt_bind_param($stmt, 's', $username);
@@ -82,15 +82,18 @@ $query = "SELECT username, status, email, banned, kills, wins FROM users WHERE r
 
 if ($stmt = $mysqli->prepare($query)) {
     $stmt->execute();
-    $stmt->bind_result($username, $status, $email, $banned, $kills, $wins);
+    $stmt->bind_result($username, $status, $email, $banned, $earthquake, $fire, $gas, $electricity, $ourgame);
     while ($stmt->fetch()) {
      echo "{";
 	 echo '"status": "' . $status . '",';
         echo '"username": "' . $username . '",';
         echo '"email": "' . $email . '",';
         echo '"banned": "' . $banned . '",';
-        echo '"kills": "' . $kills . '",';
-        echo '"wins": "' . $wins . '"';
+        echo '"earthquake": "' . $earthquake . '",';
+        echo '"fire": "' . $fire . '"';
+	echo '"gas": "' . $gas . '",';
+        echo '"electricity": "' . $electricity . '"';
+	echo '"ourgame": "' . $ourgame . '"';
         echo "}";
     }
 
