f3cc22dac56278f3e158871efd1cc2be9a1f42f9
aelnajjar <a.elnajjar@hotmail.com>
2017-02-27 22:16:54 +0400

    Update index.php

--- index.php
+++ index.php
@@ -42,15 +42,7 @@ if(isset($_POST['btn-login']))
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 	
-<nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+
           <a class="navbar-brand" href="#">VR-Based Crises Training System</a>
 <body>
 
