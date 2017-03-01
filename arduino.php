abfef6b0e9b2742aed3da227dd91f08d9836484c
aelnajjar <a.elnajjar@hotmail.com>
2017-03-01 23:50:32 +0400

    Update arduino.php

--- arduino.php
+++ arduino.php
@@ -2,10 +2,16 @@
 
    include_once 'dbconnect.php';
 
-    $query = "INSERT INTO users2 (earthquake) VALUES ('".$_GET["earthquake"]."')";    
+     // Connect to your database
+
+
+    $dbselect = mysql_select_db("vrgame",$dbconnect);
+
+    // Prepare the SQL statement
+
+    $sql = "INSERT INTO vrgame.users2 (earthquake) VALUES ('".$_GET["earthquake"]."')";    
 
     // Execute SQL statement
 
-    mysql_query($query);
-
+    mysql_query($sql);
 ?>
