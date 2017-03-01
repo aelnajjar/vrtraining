f99b622ddaa863eb2eaa8a86dd3e74d192ebab07
aelnajjar <a.elnajjar@hotmail.com>
2017-03-02 00:00:11 +0400

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -7,7 +7,16 @@
 	 
 	 $MySQLi_CON = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 
+$dbconnect = mysql_pconnect($DB_host, $DB_user, $DB_pass);
+    $dbselect = mysql_select_db("vrgame",$dbconnect);
 
+    // Prepare the SQL statement
+
+    $query = "INSERT INTO vrgame.users2 (earthquake) VALUES ('".$_GET["earthquake"]."')";    
+
+    // Execute SQL statement
+
+    
  mysql_query($query);
 
      if($MySQLi_CON->connect_errno)
