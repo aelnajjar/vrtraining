6d0baac8f5aeabe238441b6e6272aff17db4390b
aelnajjar <a.elnajjar@hotmail.com>
2017-03-03 19:35:49 +0400

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -13,7 +13,7 @@ $dbconnect = mysql_pconnect($DB_host, $DB_user, $DB_pass);
     // Prepare the SQL statement
 
   //  $query = "INSERT INTO vrgame.users2 (uid,earthquake) VALUES ('11','".$_GET["earthquake"]."')";    this will add new row with id=11 and earthquake equal to what u will type
-$query = "UPDATE users2 SET earthquake='".$_GET["earthquake"]."' WHERE username='".$_GET["username"]."'";
+$query = "UPDATE users2 SET fire='".$_GET["fire"]."' WHERE username='".$_GET["username"]."'";
 
     // Execute SQL statement
 
