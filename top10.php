4deba75f0d04941334b0d581b44b751f582b74f3
aelnajjar <a.elnajjar@hotmail.com>
2017-03-05 16:40:46 +0400

    Update top10.php

--- top10.php
+++ top10.php
@@ -52,6 +52,38 @@ $MySQLi_CON->close();
       </div>
     </nav>
 
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+     border: 1px solid black;
+}
+</style>
+</head>
+<body>
 
+<?php
+
+
+$sql = "SELECT uid, username, ourgame FROM users2";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     echo "<table><tr><th>#</th><th>User Name</th></tr>";
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["uid"]. "</td><td>" . $row["User name"]. " " . $row["ourgame"]. "</td></tr>";
+     }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+
+$conn->close();
+?>  
+
+</body>
+</html>
 
 
