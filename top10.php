2082cb2a4272c8e2ac6c1f013ec65d47489a9a3f
aelnajjar <a.elnajjar@hotmail.com>
2017-03-05 12:09:57 +0400

    Update top10.php

--- top10.php
+++ top10.php
@@ -1,10 +1,66 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+if(!isset($_SESSION['userSession']))
+{
+	header("Location: index.php");
+}
+$query = $MySQLi_CON->query("SELECT * FROM users2 WHERE uid=".$_SESSION['userSession']);
+$userRow=$query->fetch_array();
+$MySQLi_CON->close();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome - <?php echo $userRow['email']; ?></title>
+
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
+
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">VR-Based Crises Training System</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Top 10</a></li>
+           
+		  <li><a href="home.php?home"><span class="glyphicon glyphicon-home"></span>&nbsp; Home</a></li>
+            <li><a href="#">Friends</a></li>
+		  <li><a href="#">Records</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+		  
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp; <?php echo $userRow['username']; ?></a></li>
+            <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp; Logout</a></li>
+		 
+		  
+          </ul>
+        </div><!--/.nav-collapse --> 
+      </div>
+    </nav>
+		
+	<div class="container" style="margin-top:150px;text-align:center;font-family:Verdana, Geneva, sans-serif;font-size:35px;">
+	Your Records</a><br /><br />
 <!DOCTYPE html>
 <html>
 <body>
 
 <?php
 echo "<table style='border: solid 1px black;'>";
-  echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+  echo "<tr><th>#</th><th>User</th><th>Score</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator { 
      function __construct($it) { 
@@ -24,23 +80,8 @@ class TableRows extends RecursiveIteratorIterator {
      } 
 } 
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDBPDO";
 
-try {
-     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests"); 
-     $stmt->execute();
 
-     // set the resulting array to associative
-     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-
-     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-         echo $v;
-     }
 }
 catch(PDOException $e) {
      echo "Error: " . $e->getMessage();
