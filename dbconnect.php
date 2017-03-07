caea2069f40e00ee1f535f9c329a3de5b4288f9c
aelnajjar <a.elnajjar@hotmail.com>
2017-03-07 23:35:31 +0400

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,9 @@
 <?php
 
-	 // PUT IT HEREEEEEE
+	 	 $DB_host = "ap-cdbr-azure-east-a.cloudapp.net";
+	 $DB_user = "b3945d7f4038ce";
+	 $DB_pass = "4561cc15";
+	 $DB_name = "vrgame";
 	
 	 
 	 $MySQLi_CON = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
