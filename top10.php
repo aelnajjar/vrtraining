065009710fc41bd94bb5c00ddb516142375d29c3
aelnajjar <a.elnajjar@hotmail.com>
2017-03-08 00:13:24 +0400

    Update top10.php

--- top10.php
+++ top10.php
@@ -60,10 +60,7 @@ $MySQLi_CON->close();
 	
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	
-<html>
-<head>
 
-<style type="text/css">
 	
 <?php
 echo "<table style='border: solid 1px black;'>";
