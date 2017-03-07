3a06c2eed8733e5e005e6893e85daa49d2474245
aelnajjar <a.elnajjar@hotmail.com>
2017-03-07 23:54:14 +0400

    Update top10.php

--- top10.php
+++ top10.php
@@ -54,4 +54,50 @@ $MySQLi_CON->close();
         </div><!--/.nav-collapse --> 
       </div>
     </nav>
+<div class="container" style="margin-top:150px;text-align:center;font-family:Verdana, Geneva, sans-serif;font-size:35px;">
+	TOP 10</a><br /><br />
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Number</th><th>User</th><th>Score</th></tr>";
 
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+
+	 /*$DB_host = "ap-cdbr-azure-east-a.cloudapp.net";
+	 $DB_user = "b3945d7f4038ce";
+	 $DB_pass = "4561cc15";
+	 $DB_name = "vrgame";*/
+
+try {
+    $conn = new PDO("mysql:host=$DB_host;dbname=$DB_name", $DB_user,  $DB_pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT uid, username, ourgame FROM users2"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
